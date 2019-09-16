29146949bbbfa9ba6993ff3a197f2c039cf22a61
edczjw <347350971@qq.com>
2019-09-16 18:18:06 +0800

    p

---
+++ pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		手机号码将作为登录用户名使用
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'yidai'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
