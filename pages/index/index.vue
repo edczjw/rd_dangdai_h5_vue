04b4716e78173da0b001ba9195deb6113fba53b5
edczjw <347350971@qq.com>
2019-09-17 15:15:44 +0800

    2

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,52 @@
 <template>
-	<view class="content">
+	<view>
 		<!-- <image class="logo" src="/static/logo.png"></image> -->
-		手机号码将作为登录用户名使用
-		<input class="uni-input" focus placeholder="自动获得焦点" />
+		<cu-custom bgColor="bg-gradual-blue">
+			<block slot="content">易贷</block>
+		</cu-custom>
+		
+		<view class="solids-bottom padding-xs flex align-center">
+			<view class="flex-sub text-center">
+				<view class="solid-bottom text-sm">
+					<view class="text-left padding"><text class="text-grey text-left">手机号码将作为登录用户名使用</text></view>
+				</view>
+			</view>
+		</view>
+		
+		
+		<form>
+			<view class="cu-form-group">
+				<view class="title">手机号码</view>
+				<input placeholder="请输入手机号" name="input"></input>
+				<view class="cu-capsule radius">
+					<view class='cu-tag bg-blue '>
+						+86
+					</view>
+					<view class="cu-tag line-blue">
+						中国大陆
+					</view>
+				</view>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">验证码</view>
+				<input placeholder="请输入验证码" name="input"></input>
+				<button class='cu-btn bg-green shadow'>验证码</button>
+			</view>
+			
+			<view class="cu-form-group">
+				<view class="title">随机数</view>
+				<input placeholder="请输入如下随机数" name="input"></input>
+			</view>
+			
+			<view class="cu-form-group">
+				<view>
+					<label>
+						<checkbox value="cb" checked="true" color="#FFCC33" style="transform:scale(0.7)" />我已阅读、知悉并同意<a href="#">《用户协议》</a>
+					</label>
+				</view>
+			</view>
+		</form>
 	</view>
 </template>
 
@@ -10,42 +54,19 @@
 	export default {
 		data() {
 			return {
-				title: 'yidai'
+				title: 'yidai',
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	.cu-form-group .title {
+		min-width: calc(4em + 15px);
 	}
 </style>
