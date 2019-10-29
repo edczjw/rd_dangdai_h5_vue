30f87c3480d2cc6098b3a8ccc5b237e7c34ecbf0
edczjw <347350971@qq.com>
2019-10-29 10:38:01 +0800

    2

--- pages/linesapply/linesapplyindex.vue
+++ pages/linesapply/linesapplyindex.vue
@@ -6,15 +6,19 @@
 			<block slot="content">民易贷</block>
 		</cu-custom>
 		
-		<view class="">
+		<view class="bg-do">
+		<view class="top-img">
 			<image src="../../static/header_banner.png" mode=""></image>
 		</view>
 		
-		<view class="">
+		<view class="po-img">
 			<image src="../../static/px_bg.png" mode=""></image>
+			<button class="ho-button" @click="apply">立即借款</button>
 		</view>
 		
-		<button class="cu-btn bg-black margin-tb-sm round lg" @click="apply">抢先申请</button>
+		</view>
+		
+		
 	</view>
 </template>
 
@@ -44,4 +48,39 @@
 </script>
 
 <style>
+	.bg-do{
+		background: #FF3A22;
+		padding-bottom: 100%;
+		width: 100%;
+	}
+	.po-img{
+		width: 100%;
+		text-align: center;
+		position: absolute;
+		top: 750upx;
+	}
+	.po-img image{
+		width: 702upx;
+		height: 528upx;
+	}
+	.top-img image{
+		width: 100%;
+		height: 800upx;
+	}
+	.ho-button{
+		position: absolute;
+		top: 300upx;
+		left: 98upx;
+		width: 554upx;
+		height: 96upx;
+		line-height: 96upx;
+		font-family: PingFangSC-Medium;
+		font-size: 18px;
+		color: #FF7D00;
+		letter-spacing: -0.2px;
+		text-align: center;
+		background-image: linear-gradient(90deg, #FFE6B3 3%, #FFD669 100%);
+		box-shadow: 0 4px 0 0 #FBB255;
+		border-radius: 24px;
+	}
 </style>
